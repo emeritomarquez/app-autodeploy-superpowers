9d8144c72f524c818b9251fca0eac32647013ebf
Ubuntu <ubuntu@ip-172-31-1-88.us-west-2.compute.internal>
2023-03-30 14:15:18 +0000

    First Build today

--- .circleci/ansible/inventory2.py
+++ .circleci/ansible/inventory2.py
@@ -52,10 +52,10 @@ for line in lines:
     elif line == "172.31.20.231\n":
         pass
 #Windows AWS Work Machine Public
-    elif line == "35.86.145.45\n":
+    elif line == "54.202.49.190\n":
         pass
 #Ubuntu122.04 Public
-    elif line == "34.215.123.117\n":
+    elif line == "54.201.240.239\n":
         pass
 #typeorm2
     elif line == "54.149.54.106\n":
