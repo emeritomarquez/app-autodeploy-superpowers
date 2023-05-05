49abc1b6cbb4f63bd299f267cb7e29a717c0bdb3
Ubuntu <ubuntu@ip-172-31-1-88.us-west-2.compute.internal>
2023-05-05 11:53:47 +0000

    Non deployments jobs in branch udapeople-pem

--- .circleci/ansible/inventory2.py
+++ .circleci/ansible/inventory2.py
@@ -52,10 +52,10 @@ for line in lines:
     elif line == "172.31.20.231\n":
         pass
 #Windows AWS Work Machine Public
-    elif line == "35.86.77.29\n":
+    elif line == "35.162.118.123\n":
         pass
 #Ubuntu122.04 Public
-    elif line == "35.90.158.60\n":
+    elif line == "34.222.84.173\n":
         pass
 #typeorm2
     elif line == "54.213.45.43\n":
